725f3f0ac623bde73deb6bc60cfb220809b0f586
Meysam Valiolahi <mvaliolahi@gmail.com>
2024-02-28 16:42:26 +0330

    Fix routes

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use Mvaliolahi\Auth\Http\Controllers\AuthController;
 
 Route::prefix('auth')->middleware(['web'])->group(function () {
-    Route::redirect('/', '/auth/login', 301);;
-    Route::get('/login', 'Mvaliolahi\Auth\Http\Controllers\AuthController@loginForm')->name('auth.login');
-    Route::post('/logout', 'Mvaliolahi\Auth\Http\Controllers\AuthController@logout')->middleware('auth')->name('auth.logout');
-    Route::post('/send-token', 'Mvaliolahi\Auth\Http\Controllers\AuthController@sendToken')->middleware('throttle:5')->name('auth.send.token');
-    Route::get('/verify/{mobile}', 'Mvaliolahi\Auth\Http\Controllers\AuthController@verifyTokenForm')->name('auth.verify.form');
-    Route::post('/verify-token', 'Mvaliolahi\Auth\Http\Controllers\AuthController@verify')->name('auth.verify')->middleware('throttle:5');
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('auth.login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('auth.logout');
+    Route::post('/send-token', [AuthController::class, 'endToken'])->middleware('throttle:60,1')->name('auth.send.token');
+    Route::get('/verify/{mobile}',  [AuthController::class, 'verifyTokenForm'])->name('auth.verify.form');
+    Route::post('/verify-token', [AuthController::class, 'verify'])->name('auth.verify')->middleware('throttle:60,1');
 });
